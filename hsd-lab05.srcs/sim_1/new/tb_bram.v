196e7442d2f9f7c320dc2178909112a70832dccb
ralphnation <ofbyforralph@gmail.com>
2022-04-08 22:50:56 +0900

    complete tb_bram

--- hsd-lab05.srcs/sim_1/new/tb_bram.v
+++ hsd-lab05.srcs/sim_1/new/tb_bram.v
@@ -3,75 +3,73 @@
 module tb_bram();
 
     parameter BRAM_ADDR_WIDTH = 15;
+    parameter INIT_FILE = "input.txt";
+    parameter OUT_FILE = "output.txt";
 
     //for my IP
-    reg [BRAM_ADDR_WIDTH-1:0] BRAM1_ADDR;
-    reg [BRAM_ADDR_WIDTH-1:0] BRAM2_ADDR;
-    reg BRAM1_CLK;
-    reg BRAM2_CLK;
+    reg [BRAM_ADDR_WIDTH-1:0] BRAM_ADDR;
+    reg BRAM_CLK;
     reg [31:0] BRAM1_WRDATA;
     reg [31:0] BRAM2_WRDATA;
-    reg [31:0] BRAM1_RDDATA;
-    reg [31:0] BRAM2_RDDATA;
+    wire [31:0] BRAM1_RDDATA;
+    wire [31:0] BRAM2_RDDATA;
     reg BRAM1_EN;
     reg BRAM2_EN;
     reg BRAM1_RST;
     reg BRAM2_RST;
     reg [3:0] BRAM1_WE;
     reg [3:0] BRAM2_WE;
-    reg done1;
-    reg done2;
+    reg done;
 
     //for test
 
     integer i;
 
     initial begin
-        BRAM1_ADDR <= 0;
-        BRAM1_WRDATA <= 0;
-        BRAM1_RDDATA <= 0;
-        BRAM1_CLK <= 0;
-        BRAM2_CLK <= 0;
+        BRAM_CLK <= 0;
         BRAM1_EN <= 0;
         BRAM2_EN <= 0;
         BRAM1_RST <= 0;
         BRAM2_RST <= 0;
         BRAM1_WE <= 0;
         BRAM2_WE <= 0;
-        done1 <= 0;
-        done2 <= 0;
+        done <= 0;
         #30;
         BRAM1_EN <= 1;
-        BRAM1_WE <= 4'b1111;
-        
+        BRAM1_WE <= 4'b0000;
+        BRAM2_EN <= 1;
+        BRAM2_WE <= 4'b1111;
+        BRAM_ADDR <= 0;
         for (i = 0; i < 2**13; i = i + 1) begin
-            BRAM1_ADDR = BRAM1_ADDR + 1;
-            BRAM1_WRDATA = BRAM1_WRDATA + 1;
+            BRAM_ADDR = BRAM_ADDR + 4;
+            #20;
+            BRAM2_WRDATA = BRAM1_RDDATA;            
         end
+        done <= 1;
     end
     
-    always #5 BRAM1_CLK = ~BRAM1_CLK;
+    always #5 BRAM_CLK = ~BRAM_CLK;
 
-    my_bram BRAM1 (
-        .BRAM_ADDR(BRAM1_ADDR),
-        .BRAM_CLK(BRAM1_CLK),
+    my_bram #(INIT_FILE) BRAM1 (
+        .BRAM_ADDR(BRAM_ADDR),
+        .BRAM_CLK(BRAM_CLK),
         .BRAM_WRDATA(BRAM1_WRDATA),
         .BRAM_RDDATA(BRAM1_RDDATA),
         .BRAM_EN(BRAM1_EN),
         .BRAM_RST(BRAM1_RST),
         .BRAM_WE(BRAM1_WE),
-        .done(done1)
+        .done(done)
     );
 
-    /* my_bram BRAM2 (
-        .BRAM_ADDR(BRAM2_ADDR),
-        .BRAM_CLK(BRAM2_CLK),
+    my_bram #(OUT_FILE) BRAM2 (
+        .BRAM_ADDR(BRAM_ADDR),
+        .BRAM_CLK(BRAM_CLK),
         .BRAM_WRDATA(BRAM2_WRDATA),
         .BRAM_RDDATA(BRAM2_RDDATA),
         .BRAM_EN(BRAM2_EN),
         .BRAM_RST(BRAM2_RST),
         .BRAM_WE(BRAM2_WE),
-        .done(done2)
-    ); */
+        .done(done)
+    );
 
 endmodule
